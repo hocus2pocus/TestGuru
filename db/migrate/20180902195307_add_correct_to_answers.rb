ed44bfd4b8c3f67e61929e9bea9b64cedf9f66df
eugene <eugene_i@outlook.com>
2018-09-02 23:02:49 +0300

    Added boolean-value for answers, default-value for test-level

---
+++ db/migrate/20180902195307_add_correct_to_answers.rb
@@ -0,0 +1,10 @@
+class AddCorrectToAnswers < ActiveRecord::Migration[5.2]
+  def change
+    add_column(
+      :answers,
+      :correct,
+      :boolean,
+      default: 0
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_193348) do
+ActiveRecord::Schema.define(version: 2018_09_02_195307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_193348) do
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct", default: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -37,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_193348) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 1
     t.string "category_id"
     t.string "integer"
     t.datetime "created_at", null: false
