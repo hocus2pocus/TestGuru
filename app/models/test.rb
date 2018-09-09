6fdce4a282fa04ad93efcce67c5b97806bd39f90
eugene <eugene_i@outlook.com>
2018-09-09 23:23:44 +0300

    Minor style changes in the Test and User models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,8 @@
 class Test < ApplicationRecord
-  # Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента).
-
-  def self.sort_test_by_category(category)
+  def self.sorted_by_category(category)
     self.joins('JOIN categories ON tests.category_id = categories.id')
-    .where('categories.title = ?', category)
-    .order(id: :DESC)
-    .pluck(:title)
-  end
-
-  class << self
-    alias :stbc :sort_test_by_category
+      .where(categories: { title: category })
+      .order(id: :DESC)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins('JOIN results ON results.test_id = tests.id').where('user_id = ? AND level = ?', self.id, level)
+    Test.joins('JOIN results ON results.test_id = tests.id').where(user_id: self.id, level: level)
   end
-
-  alias_method :tbl, :tests_by_level
 end
-
-# Создайте инстанс-метод в модели User, который принимает в качестве аргумента значение уровня сложности и возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь на этом уровне сложности
