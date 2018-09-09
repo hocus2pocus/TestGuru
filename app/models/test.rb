cb15e5a71f8a948ce2c5119f660b8ae8c6c811bc
eugene <eugene_i@outlook.com>
2018-09-09 12:05:20 +0300

    Created method sort_test_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  # Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента).
+
+  def self.sort_test_by_category(category)
+    self.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category)
+  end
+
+  class << self
+    alias :stbc :sort_test_by_category
+  end
 end
