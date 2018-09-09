2e006288e47dfb8ab275de5dbf3f285c7ed72c80
eugene <eugene_i@outlook.com>
2018-09-09 12:13:03 +0300

    Fixed sorting in sort_test_by_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,10 @@ class Test < ApplicationRecord
   # Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов у которых Категория называется определённым образом (название категории передается в метод в качестве аргумента).
 
   def self.sort_test_by_category(category)
-    self.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category)
+    self.joins('JOIN categories ON tests.category_id = categories.id')
+    .where('categories.title = ?', category)
+    .order(id: :DESC)
+    .pluck(:title)
   end
 
   class << self
