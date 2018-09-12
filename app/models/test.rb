4d94a7915234f75f0268a68c26096f9cf2c47e5d
eugene <eugene_i@outlook.com>
2018-09-13 01:04:04 +0300

    Created simple associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+
   def self.sorted_by_category(category)
     self.joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category })
-      .order(id: :DESC)
+      .order(id: :desc)
       .pluck(:title)
   end
 end
