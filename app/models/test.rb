407471018cfb975b86deaa0fd0a719a440606d4c
eugene <eugene_i@outlook.com>
2018-09-13 01:23:58 +0300

    Created Test-User associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
 
   def self.sorted_by_category(category)
     self.joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tests_users
+  has_many :tests, through: :tests_users
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests_users.test_id = tests.id').where(tests_users: {user_id: self.id}, level: level)
   end
