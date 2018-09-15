ef30f62aaf511cd8ebbca404ec0d239a9dd24f74
eugene <eugene_i@outlook.com>
2018-09-15 16:55:53 +0300

    Corrected author-test association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :tests, class_name: "Test", foreign_key: "author_id"
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests_users.test_id = tests.id').where(tests_users: {user_id: self.id}, level: level)
