84f0efe2fe8651bbfdd01aece3f827bac104b7f2
eugene <eugene_i@outlook.com>
2018-09-15 17:09:29 +0300

    Added dependencies to associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
 
