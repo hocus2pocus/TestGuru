4c530832db66e37d1fdb7fb5aa3f8a7d444c42c4
eugene <eugene_i@outlook.com>
2018-10-19 11:33:37 +0300

    Created test_passages model and controller, dropped the tests_users model

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
   has_many :questions, dependent: :destroy
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :tests_passeges
+  has_many :users, through: :tests_passeges
 
   validates :title, presence: true, uniqueness: { scope: :level }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :tests_passeges
+  has_many :tests, through: :tests_passeges
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
   validates :email, presence: true
