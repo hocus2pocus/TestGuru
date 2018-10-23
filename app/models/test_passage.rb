07e2b5e5a165dcf9d8113b192840657f791e0ba8
eugene <eugene_i@outlook.com>
2018-10-23 23:56:37 +0300

    Improved callback to set current question in TestPassage model

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,25 +3,21 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_question
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
@@ -36,6 +32,10 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    if current_question
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    else
+      test.questions.first if test.present?
+    end
   end
 end
