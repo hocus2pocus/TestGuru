0c97204cb1875c31ee8d8bdd4015b8e1bef04c0a
eugene <eugene_i@outlook.com>
2019-01-08 23:49:19 +0300

    Created password_digest column, removed password and login columns from users model

---
+++ db/migrate/20190108204348_remove_password_and_login_from_users.rb
@@ -0,0 +1,6 @@
+class RemovePasswordAndLoginFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :login, :string
+    remove_column :users, :password, :string
+  end
+end
