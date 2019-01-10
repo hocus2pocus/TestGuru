5ded20b735a42f78ba3e8dd810c34f096e821640
eugene <eugene_i@outlook.com>
2019-01-10 13:45:32 +0300

    Improved authorization functionality with rails inbuilt methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,10 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
-
-  attr_reader :password
-  attr_writer :password_confirmation
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
-  validates :email, presence: true
-  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
@@ -21,24 +14,5 @@ class User < ApplicationRecord
   def test_passage(test)
     #ordered to take exactly the last test that user passed
     test_passages.order(id: :desc).find_by(test: test)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
