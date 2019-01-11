975f24d14d2f79c52d1cce9ba736f9594dcff3b7
eugene <eugene_i@outlook.com>
2019-01-11 18:05:30 +0300

    Prohibited actions for non-logged in users

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,4 @@
 class TestsController < ApplicationController
-  before_action :authenticate_user!
   before_action :find_test, only: %i[show edit update destroy start]
   before_action :find_user, only: :start
 
@@ -56,6 +55,6 @@ class TestsController < ApplicationController
   end
 
   def find_user
-    @user = User.first
+    @user = User.find(session[:user_id])
   end
 end
