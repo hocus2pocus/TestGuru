c27f8af331ff8f1edd979394f377813fc72131f3
eugene <eugene_i@outlook.com>
2019-02-12 22:13:13 +0300

    Integrated ru-localization'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
   before_action :authenticate_user!
   before_action :configure_permited_parameters, if: :devise_controller?
 
@@ -12,10 +13,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
   private
 
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
