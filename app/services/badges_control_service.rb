5f5c0fbacb63175f168980513f8d8899e5582410
eugene <eugene_i@outlook.com>
2019-05-07 21:09:27 +0300

    Created Badges Control Service

---
+++ app/services/badges_control_service.rb
@@ -0,0 +1,27 @@
+class BadgesControlService
+
+  def initialize(test_passage)
+    @test_passage = test_passage
+    @user = test_passage.user
+
+    badges_model_check
+  end
+
+  def call
+  end
+
+  # private
+
+  def badges_model_check
+    default_bages
+    @first_attempt ||= Badge.create!(title: 'First attempt', picture: "ei-trophy")
+  end
+
+  def default_bages
+    @first_attempt = Badge.find_by(title: 'First attempt')
+  end
+
+  def rule_first_attempt
+    TestPassage.where(user_id: @user.id, test_id: @test_passage.test.id).count == 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_065146) do
+ActiveRecord::Schema.define(version: 2019_05_07_152614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_03_11_065146) do
     t.datetime "updated_at", null: false
     t.boolean "correct", default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "picture", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
