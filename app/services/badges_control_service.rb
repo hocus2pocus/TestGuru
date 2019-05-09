ffb1efbf4ed79b787a08b88d21d848f220b3dbcf
eugene <eugene_i@outlook.com>
2019-05-09 23:56:22 +0300

    Created button for badges index, minor style changes

--- app/services/badges_control_service.rb
+++ app/services/badges_control_service.rb
@@ -15,13 +15,23 @@ class BadgesControlService
   def badges_model_check
     default_bages
     @first_attempt ||= Badge.create!(title: 'First attempt', picture: "red-badge")
+    @all_by_category ||= Badge.create!(title: 'All by category', picture: "green-badge")
+    @all_by_level ||= Badge.create!(title: 'All by level', picture: "pink-badge")
   end
 
   def default_bages
     @first_attempt = Badge.find_by(title: 'First attempt')
+    @all_by_category = Badge.find_by(title: 'All by category')
+    @all_by_level = Badge.find_by(title: 'All by level')
   end
 
   def rule_first_attempt
     TestPassage.where(user_id: @user.id, test_id: @test_passage.test.id).count == 1
+  end
+
+  def rule_all_by_category
+  end
+
+  def rule_all_by_level
   end
 end
